22b0e91877d22c61b96679e612c9bdeb0c718459
huabo <huabo1900@163.com>
2017-06-14 11:35:16 +0800

    sso认证

--- bobo-upms/bobo-upms-admin/src/main/java/com/bobo/upms/admin/controller/IndexController.java
+++ bobo-upms/bobo-upms-admin/src/main/java/com/bobo/upms/admin/controller/IndexController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by huabo on 2017/5/27.
  */
 @Controller
-
 public class IndexController {
     @RequestMapping("/index")
     public String index(Model model) {
